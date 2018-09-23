b4af245c2132c3317e5c2d53d63cac8fe4436d56
William Zhang <zhangwilliam2008@hotmail.com>
2018-09-23 18:36:19 -0400

    Your Message

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start() {
+		
+	}
+	
+	// FixedUpdate is called at a constant rate
+	void FixedUpdate() {
+		
+	}
+		
+}
