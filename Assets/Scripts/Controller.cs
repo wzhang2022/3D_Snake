c92c1ccb3fab098278df5b4c9d27c9a093fe332c
wzhang2022 <43010874+wzhang2022@users.noreply.github.com>
2018-10-07 19:50:27 -0400

    Added project
    -

---
+++ Assets/Scripts/Controller.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour {
+    public Vector3 direction;
+    public GameObject head;
+    public GameObject prefab;
+	// Use this for initialization
+	void Start () {
+        Invoke("Repeat", 0.5f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+    }
+
+    void Repeat() {
+        Move();
+    }
+
+    void Move() {
+        GameObject new_object = Instantiate(prefab);
+        new_object.transform.Translate(direction);
+        head = new_object;
+    }
+}
