79633de641e2604086e4bd28771dde6e3cd11de3
wzhang2022 <43010874+wzhang2022@users.noreply.github.com>
2018-10-14 19:09:01 -0400

    Added 2_layer scene
    Added 2_layer scene

--- Assets/Scripts/Controller_Two_layers.cs
+++ Assets/Scripts/Controller_Two_layers.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class Controller : MonoBehaviour {
+public class Controller_Two_layers : MonoBehaviour {
     public Vector3 direction;
     public GameObject head;
     public GameObject prefab;
@@ -18,46 +18,63 @@ public class Controller : MonoBehaviour {
         public char downX;
     }
     public KeyBind keyBind;
+
+    private int layer = 0;
+    private Vector3 direction2D;
 	// Use this for initialization
 	void Start () {
         InvokeRepeating("Repeat", 0.5f, speed);
+        direction2D = direction;
 	}
 	
 	// Update is called once per frame
 	void Update () {
         foreach (char c in Input.inputString) {
-            if (c ==  keyBind.downX && direction != Vector3.right) {
+            if (c == keyBind.downX && direction != Vector3.right) {
                 direction = Vector3.left;
+                direction2D = direction;
             } else if (c == keyBind.upX && direction != Vector3.left) {
                 direction = Vector3.right;
-            } else if (c == keyBind.downY && direction != Vector3.up) {
-                direction = Vector3.down;
-            } else if (c == keyBind.upY && direction != Vector3.down) {
-                direction = Vector3.up;
+                direction2D = direction;
             } else if (c == keyBind.downZ && direction != Vector3.forward) {
                 direction = Vector3.back;
+                direction2D = direction;
             } else if (c == keyBind.upZ && direction != Vector3.back) {
                 direction = Vector3.forward;
+                direction2D = direction;
+            } else if (c == keyBind.downY && direction != Vector3.up && layer == 1) {
+                direction = Vector3.down;
+            } else if (c == keyBind.upY && direction != Vector3.down && layer == 0) {
+                direction = Vector3.up;
             }
         }
     }
 
     void Repeat() {
+        if (direction == Vector3.up) {
+            if (layer == 0) {
+                layer = 1;
+            }  else if (layer >= 1){
+                direction = direction2D;
+            }
+        } else if (direction == Vector3.down) {
+            if (layer == 1) {
+                layer = 0;
+            } else if (layer <= 0){
+                direction = direction2D;
+            }
+        }
         Move();
-        CheckCollision();
     }
 
     void Move() {
         GameObject new_object = Instantiate(prefab, head.transform.position, head.transform.rotation);
         head.transform.Translate(direction);
-    }
-    
-    void CheckCollision() {
 
     }
 
     private void OnTriggerEnter(Collider other) {
 
-        SceneManager.LoadScene((SceneManager.GetActiveScene().buildIndex + 1)%2);
+        SceneManager.LoadScene(0);
     }
 }
--- Assets/Scripts/StartMenu.cs
+++ Assets/Scripts/StartMenu.cs
@@ -7,6 +7,6 @@ public class StartMenu : MonoBehaviour {
 
 	public void PlayGame()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        SceneManager.LoadScene(2);
     }
 }
