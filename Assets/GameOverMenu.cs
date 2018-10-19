b3079c3f832b52e84ed3ae5d488c3d0dc812b6c1
kendallzhu <kzhu@college.harvard.edu>
2018-10-19 00:34:44 -0400

    removed useless code

--- Assets/GameOverMenu.cs
+++ Assets/GameOverMenu.cs
@@ -4,25 +4,13 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class GameOverMenu : MonoBehaviour {
-    public void DeactivateMenu()
-    {
-        this.gameObject.transform.parent.gameObject.SetActive(false);
-        for (int i = 0; i < this.gameObject.transform.childCount; i++)
-        {
-            var child = this.gameObject.transform.GetChild(i).gameObject;
-            if (child.name.Contains("Text"))
-                child.SetActive(false);
-        }
-    }
     public void PlayAgain()
     {
-        DeactivateMenu();
         SceneManager.LoadScene(1);
         Time.timeScale = 1;
     }
     public void MainMenu()
     {
-        DeactivateMenu();
         Time.timeScale = 1;
         SceneManager.LoadScene(0);
     }
