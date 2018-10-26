2f90cf8d42dbb56c4a120c0a01c320c3940ac796
kendallzhu <kzhu@college.harvard.edu>
2018-10-26 12:07:31 -0400

    basic refactor

---
+++ Assets/Scripts/MatchManager.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+// TODO: will add another script "GameManager" for things requiring persisting over scenes, like music, etc.
+public class MatchManager : MonoBehaviour
+{
+    // game constants
+    public float timeStep = 0.2f;
+
+    // player controllers - note can generalize into array if we do multiplayer
+    public PlayerController player1;
+    public PlayerController player2;
+
+    // end game menu objects
+    public GameObject gameOverMenu;
+    public GameObject player1WinsText;
+    public GameObject player2WinsText;
+    public GameObject tieText;
+
+    // TODO: create a map class - script that generates walls precisely & returns their locations
+    // public Map map;
+    // Temporary solution - rectangular walls (not responsive to actual wall objects)
+    public int minX = -10;
+    public int maxX = 10;
+    public int minY = -10;
+    public int maxY = 10;
+
+    // store all game data
+    private HashSet<Vector3> playerPositions = new HashSet<Vector3>();
+    private HashSet<Vector3> wallPositions = new HashSet<Vector3>();
+    // HashSet<Vector3> foodPositions = new HashSet<Vector3>();
+
+    // Initialize everything
+    void Start()
+    {
+        InvokeRepeating("Repeat", 0.5f, timeStep);
+        gameOverMenu.SetActive(false);
+        // Temporary solution - initialize hard-coded walls NEED FIX!!!
+        for (int x = minX-1; x < maxX; x++)
+        {
+            wallPositions.Add(new Vector3(x, maxY + 1, 0));
+            wallPositions.Add(new Vector3(x, maxY + 1, 1));
+            wallPositions.Add(new Vector3(x, minY - 1, 0));
+            wallPositions.Add(new Vector3(x, minY - 1, 1));
+        }
+        playerPositions.Add(player1.head.transform.position);
+        playerPositions.Add(player2.head.transform.position);
+    }
+
+    void Repeat()
+    {
+        CheckMoves();
+        player1.Move();
+        player2.Move();
+    }
+
+    void CheckMoves()
+    {
+        // modify this code for stuff other than gameOver to happen on collision
+        Vector3 nextPosition1 = player1.NextMove();
+        Vector3 nextPosition2 = player2.NextMove();
+        
+        bool player2Win = playerPositions.Contains(nextPosition1) || wallPositions.Contains(nextPosition1);
+        bool player1Win = playerPositions.Contains(nextPosition2) || wallPositions.Contains(nextPosition2);
+        bool headCollision = nextPosition1 == nextPosition2;
+        bool tie = headCollision || (player1Win && player2Win);
+
+        playerPositions.Add(nextPosition1);
+        playerPositions.Add(nextPosition2);
+
+        // if someone lost, activate game over menu with the appropriate message
+        if (player2Win || player1Win || tie)
+        {
+            Time.timeScale = 0;
+            gameOverMenu.SetActive(true);
+            if (tie)
+            {
+                tieText.SetActive(true);
+            }
+            else if (player1Win)
+            {
+                player1WinsText.SetActive(true);
+            }
+            else if (player2Win)
+            {
+                player2WinsText.SetActive(true);
+            }
+        }
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,16 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class Controller_Two_layers : MonoBehaviour {
-    public GameObject gameOverMenu;
-    public GameObject player1WinsText;
-    public GameObject player2WinsText;
-    public GameObject tieText;
-
+public class PlayerController : MonoBehaviour {
     public Vector3 direction;
     public GameObject head;
     public GameObject bodyPrefab;
-    public float speed = 0.2f;
     [System.Serializable]
     public struct KeyBind {
         public char upZ;
@@ -29,9 +23,7 @@ public class Controller_Two_layers : MonoBehaviour {
     private Vector3 direction_prev;
     // Use this for initialization
     void Start () {
-        InvokeRepeating("Repeat", 0.5f, speed);
         direction2D = direction;
-        gameOverMenu.SetActive(false);
     }
 	
 	// Update is called once per frame
@@ -57,51 +49,42 @@ public class Controller_Two_layers : MonoBehaviour {
         }
     }
 
-    void Repeat() {
-        if (direction == Vector3.up) {
-            if (layer == 0) {
+    public Vector3 NextMove()
+    {
+        if ((direction == Vector3.up && layer == 1) ||
+            (direction == Vector3.down && layer == 0))
+        {
+            return head.transform.position + direction2D;
+        }
+        return head.transform.position + direction;
+    }
+
+    public void Move() {
+        if (direction == Vector3.up)
+        {
+            if (layer == 0)
+            {
                 layer = 1;
-            }  else if (layer >= 1){
-                direction = direction2D;
             }
-        } else if (direction == Vector3.down) {
-            if (layer == 1) {
-                layer = 0;
-            } else if (layer <= 0){
+            else if (layer >= 1)
+            {
                 direction = direction2D;
             }
         }
-        Move();
-    }
-
-    void Move() {
+        else if (direction == Vector3.down)
+        {
+            if (layer == 1)
+            {
+                layer = 0;
+            }
+            else if (layer <= 0)
+            {
+                direction = direction2D;
+            }
+        }
         Vector3 oldPosition = head.transform.position;
         head.transform.Translate(direction);
         direction_prev = direction;
         GameObject new_object = Instantiate(bodyPrefab, oldPosition, head.transform.rotation);
-    }
-
-    private void OnTriggerEnter(Collider c) {
-        Debug.Log(c);
-        bool player2Win = gameObject.name.Contains("1");
-        bool player1Win = gameObject.name.Contains("2");
-        Time.timeScale = 0;
-        // activate the game over menu with the appropriate message
-        // (might not be best way to do this)
-        gameOverMenu.SetActive(true);
-        if (player1Win)
-        {
-            player1WinsText.SetActive(true);
-        }
-        if (player2Win)
-        {
-            player2WinsText.SetActive(true);
-        }
-        if (player1WinsText.activeSelf && player2WinsText.activeSelf)
-        {
-            tieText.SetActive(true);
-            player1WinsText.SetActive(false);
-            player2WinsText.SetActive(false);
-        }
     }
 }
