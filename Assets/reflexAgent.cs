c0f09ca8ad9a81f720dab74a243ddbfd0126e717
kendallzhu <kzhu@college.harvard.edu>
2018-11-24 13:55:29 -0500

    removed weird prefab link

---
+++ Assets/reflexAgent.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReflexAgent : PlayerController
+{
+    public override void Update()
+    {
+        return;
+    }
+
+    public override void DecideMove()
+    {
+        base.MoveCommand(Vector3.left);
+    }
+}
