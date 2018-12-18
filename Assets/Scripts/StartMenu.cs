b264b28969f4a19e0c818390f6da257630c0607a
wzhang2022 <43010874+wzhang2022@users.noreply.github.com>
2018-12-18 02:28:33 -0500

    Update StartMenu.cs

--- Assets/Scripts/StartMenu.cs
+++ Assets/Scripts/StartMenu.cs
@@ -6,40 +6,40 @@ using UnityEngine.SceneManagement;
 public class StartMenu : MonoBehaviour {
 
     public void WatchGreedy_GreedyMax_Basic() {
-        SceneManager.LoadScene(3);
+        SceneManager.LoadScene(5);
     }
     public void WatchGreedySearch_GreedyMax_Basic() {
-        SceneManager.LoadScene(3);
+        SceneManager.LoadScene(6);
     }
     public void WatchGreedy_Expectimax_Basic() {
-        SceneManager.LoadScene(3);
+        SceneManager.LoadScene(2);
     }
     public void WatchGreedy_GreedySearch_Basic() {
         SceneManager.LoadScene(3);
     }
     public void WatchReflex_Expectimax_Basic() {
-        SceneManager.LoadScene(3);
+        SceneManager.LoadScene(8);
     }
     public void WatchGreedy_GreedySearch_Cross() {
-        SceneManager.LoadScene(3);
+        SceneManager.LoadScene(4);
     }
     public void WatchGreedySearch_GreedyMax_Cross() {
-        SceneManager.LoadScene(3);
+        SceneManager.LoadScene(7);
     }
     public void WatchRL_Greedy_Basic() {
-        SceneManager.LoadScene(3);
+        SceneManager.LoadScene(9);
     }
     public void WatchRL_GreedySearch_Basic() {
-        SceneManager.LoadScene(3);
+        SceneManager.LoadScene(11);
     }
     public void WatchRL_GreedyMax_Basic() {
-        SceneManager.LoadScene(3);
+        SceneManager.LoadScene(10);
     }
 
     public void Play2Player()
     {
         Debug.Log("Play 2-player");
-        SceneManager.LoadScene(2);
+        SceneManager.LoadScene(1);
     }
     public void Play1Player()
     {
