b714265ddeff92ff890cbbce60102dff5da82632
wzhang2022 <43010874+wzhang2022@users.noreply.github.com>
2018-12-17 19:42:25 -0500

    fixed code

--- Assets/Scripts/MatchManager.cs
+++ Assets/Scripts/MatchManager.cs
@@ -239,6 +239,7 @@ public class MatchManager : MonoBehaviour
         if (player2Win || player1Win || tie)
         {
             Time.timeScale = 0;
+            //loop for trials
             if (loopGame) {
                 string outcome = "";
                 if (tie) {
@@ -271,7 +272,7 @@ public class MatchManager : MonoBehaviour
                 Debug.Log(SceneManager.GetActiveScene().buildIndex);
                 SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
             }
-            // Time.timeScale = 1;
+            Time.timeScale = 1;
             Debug.Log("Gameover");
             gameOverMenu.SetActive(true);
             if (tie)
